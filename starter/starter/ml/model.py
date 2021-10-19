2c4240a7e69c5ac4963d40adb9b924322ec43dc9
mrstelmach <marekstelmach89@gmail.com>
2021-10-19 12:51:37 +0200

    Add get_slices_performance fn

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -5,12 +5,12 @@ Functions for training Gradient Boosting with hyperparameter optimization,
 computing metrics and running in inference mode.
 """
 
+import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.model_selection import RandomizedSearchCV
 
 
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train, hp_iter=5):
     """
     Trains a machine learning model and returns it.
@@ -89,3 +89,61 @@ def inference(model, X):
     preds = model.predict(X)
     
     return preds
+
+
+def get_slices_performance(model, data, categorical_features, process_data_fn,
+                           one_hot_encoder, label_binarizer, label="salary"):
+    """
+    Output performance of the model on slices of the data for categorical
+    features with comparison to overall performance.
+    
+    Inputs
+    ------
+    model : ???
+        Trained machine learning model.
+    data : pd.DataFrame
+        Data for computing performance.
+    categorical_features : list
+        List of categorical features.
+    process_data_fn : function
+        Function for preprocessing data.
+    one_hot_encoder : sklearn.preprocessing.OneHotEncoder
+        Fitted instance of OneHotEncoder.
+    label_binarizer : sklearn.preprocessing.LabelBinarizer
+        Fitted instance of LabelBinarizer.
+    label : str
+        Column name with y value.
+    Returns
+    -------
+    performance : pd.DataFrame
+        Data frame with performance on slices and overall score.
+    """
+    performance = []
+    categorical_features.insert(0, 'overall')
+    data['overall'] = 'Overall'
+
+    for feature in categorical_features:
+        if 'overall' in categorical_features:
+            categorical_features.remove('overall')
+        for val in data[feature].unique():
+            idx = (data[feature] == val)
+            data_slice = data[idx].copy()
+            if 'overall' in data_slice.columns:
+                data_slice.drop(columns=['overall'], inplace=True)
+            X_test, y_test, _, _ = process_data_fn(
+                X=data_slice,
+                categorical_features=categorical_features,
+                label=label,
+                training=False,
+                encoder=one_hot_encoder, 
+                lb=label_binarizer
+            )
+            y_pred = inference(model, X_test)
+            pr, rc, fb = compute_model_metrics(y_test, y_pred)
+            performance.append({
+                'feature': feature, 'value': val, 'samples': len(data_slice),
+                'precision': pr, 'recall': rc, 'fbeta': fb
+            })
+    
+    performance = pd.DataFrame(performance)
+    return performance
