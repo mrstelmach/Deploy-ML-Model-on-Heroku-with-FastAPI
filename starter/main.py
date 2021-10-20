8604ae51b8c37fb36bde08d5d253cb95d44a805c
mrstelmach <marekstelmach89@gmail.com>
2021-10-20 13:31:45 +0200

    Initialize post method

--- starter/main.py
+++ starter/main.py
@@ -5,9 +5,34 @@ RESTful API using FastAPI with get and post methods.
 """
 
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+
+class InputFeatures(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int = Field(..., alias='education-num')
+    marital_status: str = Field(..., alias='marital-status')
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int = Field(..., alias='capital-gain')
+    capital_loss: int = Field(..., alias='capital-loss')
+    hours_per_week: int = Field(..., alias='hours-per-week')
+    native_country: str = Field(..., alias='native-country')
+
 
 app = FastAPI()
+
 
 @app.get("/")
 async def welcome_msg():
     return {'message': "Welcome to the API!"}
+
+
+@app.post("/inference")
+async def model_inference(input_features: InputFeatures):
+    pass
