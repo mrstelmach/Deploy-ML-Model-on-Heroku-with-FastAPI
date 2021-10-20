e895d497949cb6ff67aacfbbbf282e949e2fa8fa
mrstelmach <marekstelmach89@gmail.com>
2021-10-20 12:32:55 +0200

    Create get method with fastapi

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,13 @@
-# Put the code for your API here.
+# -*- coding: utf-8 -*-
+
+"""
+RESTful API using FastAPI with get and post methods.
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def welcome_msg():
+    return {'message': "Welcome to the API!"}
