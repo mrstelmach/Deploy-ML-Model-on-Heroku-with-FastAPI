77a2d36aadde8e42dd9b74687125c21b8b9752b6
mrstelmach <marekstelmach89@gmail.com>
2021-10-20 14:29:17 +0200

    Add post method to app

--- starter/main.py
+++ starter/main.py
@@ -4,8 +4,20 @@
 RESTful API using FastAPI with get and post methods.
 """
 
+import os
+import pickle
+
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+
+from starter.starter.ml.data import process_data
+from starter.starter.ml.model import inference, ModelConfig
+
+mc = ModelConfig()
+model = pickle.load(open(os.path.join(mc.model_path, 'gb_model.pkl'), 'rb'))
+encoder = pickle.load(open(os.path.join(mc.model_path, 'one_hot_encoder.pkl'), 'rb'))
+lb = pickle.load(open(os.path.join(mc.model_path, 'label_binarizer.pkl'), 'rb'))
 
 
 class InputFeatures(BaseModel):
@@ -30,9 +42,16 @@ app = FastAPI()
 
 @app.get("/")
 async def welcome_msg():
+    """Get welcome message."""
     return {'message': "Welcome to the API!"}
 
 
 @app.post("/inference")
 async def model_inference(input_features: InputFeatures):
-    pass
+    """Run model in inference mode."""
+    input_df = pd.DataFrame([input_features.dict(by_alias=True)])
+    X, _, _, _ = process_data(input_df, mc.cat_features, training=False,
+                              encoder=encoder, lb=lb)
+    prediction = inference(model, X)
+    prediction = lb.inverse_transform(prediction).item(0)
+    return {'prediction': prediction}
